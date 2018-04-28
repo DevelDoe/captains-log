3e5dd4a50d1e13039a101b6b32fb51299bb382fb
develdoe <andreeray@live.com>
2018-04-28 17:08:38 +0200

    production

---
+++ src/components/Users/Users.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+    <div id='users'>
+        <ul>
+            <li v-for="user in users">
+                {{ user.name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [ 'users' ]
+}
+</script>
+
+<style lang="css">
+</style>
